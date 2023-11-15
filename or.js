5c7e6becee4bacdf21ab74b484a1ea929427a037
kebekchala <chalakebek@gmail.com>
2023-11-15 18:13:55 +0300

    first commit

---
+++ or.js
@@ -0,0 +1,8 @@
+function showBio() {
+    var bio = document.getElementById("bio");
+    if (bio.style.display === "none") {
+      bio.style.display = "block";
+    } else {
+      bio.style.display = "none";
+    }
+  }
